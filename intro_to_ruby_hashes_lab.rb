6a1f88dc65510c6b225327cb02067be12e428c61
Kevin Hayden <kevin.fhayden@gmail.com>
2019-07-31 00:02:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,9 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  hash(:name, "Grace Hopper")
+  hash(key, value) = {
+    :name => "Grace Hopper"
+  }
 end
 
 def read_from_hash(hash, key)
